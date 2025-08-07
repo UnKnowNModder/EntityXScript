59a1974ca3798751ea0b7789efb983aab0be6479
RaZa.Py <115006831+RAZA6377@users.noreply.github.com>
2025-08-07 12:18:57 +0530

    Added match command (with bugs maybe)

--- script/dist/ba_root/mods/utilities/entitybot.py
+++ script/dist/ba_root/mods/utilities/entitybot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import bacore
 
 
 class EntityBot(commands.Bot):
@@ -22,13 +23,19 @@ class EntityBot(commands.Bot):
     match_group = app_commands.Group(name='match', description='Match Management')
 
     @match_group.command(name='add',description='Add a match')
-    async def match_add(self, interaction: discord.Interaction, match_id: int, series_count: int, team_name1: str, team_name2: str, team1_pbids: str, team2_pbids: str):
-        team1_players = team1_pbids.split(',')
-        team2_players = team2_pbids.split(',')
-        if len(team1_players) or len(team2_players) < 4:
-            await interaction.response.send_message("Enter equal players **4** in both teams")
-        else:
-            match = 
+    async def match_add(self, interaction: discord.Interaction, series_count: int, team_name1: str, team_name2: str, team1_pbids: str, team2_pbids: str):
+        try:
+            team1_players = team1_pbids.split(',')
+            team2_players = team2_pbids.split(',')
+            if len(team1_players) or len(team2_players) < 4:
+                await interaction.response.send_message("Enter equal players **4** in both teams")
+            else:
+                match = {"series": series_count, "team1": [team_name1, team1_players], "team2": [team_name2, team2_players]}
+                bacore.tournament.insert(match)
+                await interaction.response.send_message("Match Appointed")
+        except Exception as e:
+            await interaction.response.send_message(e)
+            pass
 
 def main():
     bot = EntityBot()
