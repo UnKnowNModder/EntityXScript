4edbb2f407976da238f83ba5f87d0f25146150a5
RaZa.Py <115006831+RAZA6377@users.noreply.github.com>
2025-08-07 12:12:21 +0530

    Update entitybot.py

--- script/dist/ba_root/mods/utilities/entitybot.py
+++ script/dist/ba_root/mods/utilities/entitybot.py
@@ -23,6 +23,12 @@ class EntityBot(commands.Bot):
 
     @match_group.command(name='add',description='Add a match')
     async def match_add(self, interaction: discord.Interaction, match_id: int, series_count: int, team_name1: str, team_name2: str, team1_pbids: str, team2_pbids: str):
+        team1_players = team1_pbids.split(',')
+        team2_players = team2_pbids.split(',')
+        if len(team1_players) or len(team2_players) < 4:
+            await interaction.response.send_message("Enter equal players **4** in both teams")
+        else:
+            match = 
 
 def main():
     bot = EntityBot()
