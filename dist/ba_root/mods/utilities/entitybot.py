afb39eeafffe662aa0cd42ec3c26405b84c8d69f
RaZa.Py <115006831+RAZA6377@users.noreply.github.com>
2025-08-21 13:19:56 +0530

    Update entitybot.py
    Fixed bugs for tournament command and added plugin to start the bot

--- dist/ba_root/mods/utilities/entitybot.py
+++ dist/ba_root/mods/utilities/entitybot.py
@@ -2,41 +2,51 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import bacore
+import babase
 
 
 class EntityBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="+", intents=discord.Intents.all())
+        super().__init__(command_prefix="e!", intents=discord.Intents.default())
 
     async def on_ready(self):
         print(f'Logged in as {self.user.name} - {self.user.id}')
 
     async def on_message(self, message):
+        """Check messages if sent by itself return None else process the command (if any)"""
         if message.author == self.user:
             return
         await self.process_commands(message)
-        
+     
+    """To check bot response"""
     @commands.command(name='ping')
     async def ping(self, ctx):
         await ctx.send('Pong!')
 
+    """Creates a slash command group named match so we can use commands like /match add..."""
     match_group = app_commands.Group(name='match', description='Match Management')
 
-    @match_group.command(name='add',description='Add a match')
-    async def match_add(self, interaction: discord.Interaction, series_count: int, team_name1: str, team_name2: str, team1_pbids: str, team2_pbids: str):
+    @match_group.command(name='add',description='Appoint a match')
+    async def match_add(self, interaction: discord.Interaction, series_count: int, team_name1: str, team_name2: str, team1_players: str, team2_players: str):
         try:
-            team1_players = team1_pbids.split(',')
-            team2_players = team2_pbids.split(',')
-            if len(team1_players) or len(team2_players) < 1:
-                await interaction.response.send_message("Enter equal players in both teams")
-            else:
-                match = {"series": series_count, "team1": [team_name1, team1_players], "team2": [team_name2, team2_players]}
-                bacore.tournament.insert(match)
-                await interaction.response.send_message("Match Appointed")
+            match = {}
+            match["series"] = int(series_count)
+            match["team1"] = {}
+            match["team1"][team_name1] = list(eval(team1_players))
+            match["team2"] = {}
+            match["team2"][team_name2] = list(eval(team2_players))
+            bacore.tournament.insert(match)
+            await interaction.response.send_message("Match Appointed")
         except Exception as e:
             await interaction.response.send_message(e)
             pass
 
-def main():
+def start_bot():
     bot = EntityBot()
-    bot.run()
+    bot.run("Enter Your Fuking Bot Token Here")
+
+# ba_meta import babase.Plugin
+class BotPlugin(babase.plugin):
+    def __init__(self):
+        start_bot()
+        
