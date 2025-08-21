a539849cf2baf34c9ff47c63a6b445ab560b88b2
RaZa.Py <115006831+RAZA6377@users.noreply.github.com>
2025-08-21 14:57:13 +0530

    Update entitybot.py
    Added sync command to sync slash commands

--- dist/ba_root/mods/utilities/entitybot.py
+++ dist/ba_root/mods/utilities/entitybot.py
@@ -38,6 +38,56 @@ class EntityBot(commands.Bot):
             pass
          
      
+    """Sync slash commands"""
+    @commands.command()
+    @commands.guild_only()
+    @commands.is_owner()
+    async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: typing.Optional[typing.Literal["~", "*", "^"]] = None) -> None:
+        if not guilds:
+            if spec == "~":
+
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+
+            elif spec == "*":
+                ctx.bot.tree.copy_global_to(guild=ctx.guild)
+
+                synced = await ctx.bot.tree.sync(guild=ctx.guild)
+
+            elif spec == "^":
+                ctx.bot.tree.clear_commands(guild=ctx.guild)
+
+                await ctx.bot.tree.sync(guild=ctx.guild)
+
+                synced = []
+
+            else:
+
+                synced = await ctx.bot.tree.sync()
+
+            await ctx.send(f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}")
+
+
+
+            return
+
+        ret = 0
+
+        for guild in guilds:
+
+            try:
+
+                await ctx.bot.tree.sync(guild=guild)
+
+            except discord.HTTPException:
+
+                pass
+
+            else:
+
+                ret += 1
+
+        await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
     """To check bot response"""
     @commands.command(name='ping')
     async def ping(self, ctx):
